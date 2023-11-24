03a68c866753348905e80a20e88c3b42efa125c7
Nataliia <nataliya.amons@gmail.com>
2023-11-24 22:32:41 +0200

    main pages

---
+++ src/main/java/com/example/TextGame/TextGameApplication.java
@@ -0,0 +1,25 @@
+package com.example.TextGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Controller
+public class TextGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextGameApplication.class, args);
+	}
+	//@GetMapping
+	public String hello(){
+		return "Hello World!";
+	}
+
+	@GetMapping
+	public String welcome(){
+		return "character1";
+	}
+}
