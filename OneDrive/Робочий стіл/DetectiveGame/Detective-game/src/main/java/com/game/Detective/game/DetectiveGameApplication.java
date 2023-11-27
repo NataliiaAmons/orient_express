c04361f6c70242bcd2bbad5f0c1425a1993d9ceb
sevenN0007 <c2smart049@gmail.com>
2023-11-27 16:07:45 +0200

    connected web pages with functions

---
+++ OneDrive/Робочий стіл/DetectiveGame/Detective-game/src/main/java/com/game/Detective/game/DetectiveGameApplication.java
@@ -0,0 +1,15 @@
+package com.game.Detective.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectiveGameApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DetectiveGameApplication.class, args);
+	}
+
+}
+/* посилання на сайт: http://localhost:8080/main */
