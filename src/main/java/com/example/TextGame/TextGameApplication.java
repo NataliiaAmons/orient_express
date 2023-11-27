bec2439862528797368067a6857c6856d2b8c98c
Nataliia <nataliya.amons@gmail.com>
2023-11-27 15:15:33 +0200

    reading character data and questions from txt files

--- src/main/java/com/example/TextGame/TextGameApplication.java
+++ src/main/java/com/example/TextGame/TextGameApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 @SpringBootApplication
 @Controller
 public class TextGameApplication {
@@ -14,16 +17,33 @@ public class TextGameApplication {
 	}
 
 	@GetMapping("/")
+	// function to check if getting characters and questions from file works correctly
+	public String getCharacters() throws IOException {
+	 Character[] characters = new Character().getCharactersFromFile();
+
+	 for (int i = 0; i< characters.length; i++) {
+		 System.out.printf("Character%d:\n%s\n%s\n%s\n%s\n\n", i+1, String.valueOf(characters[i].getNumber()), characters[i].getName(), characters[i].getPhoto(), characters[i].getInfo());
+		 System.out.printf("Qestions:\n");
+		 ArrayList<Question> questions = characters[i].getQuestions();
+		 for(Question index: questions){
+			 System.out.printf("question%s\n%s\n%s\n%s\n%s\n\n\n", String.valueOf(index.getNumber()), index.getEvidenceNeeded(), index.getQuestionText(), index.getAnswer(), index.getPrevious());
+		 }
+	 }
+
+		return "TEST";
+	}
+
 	public String dialog(){
-		return "character3";
+		System.out.print("TEST");
+		return "TEST";
 	}
 
-	@GetMapping("Character1.html")
+	//@GetMapping("/")
 	public String character1(){
-		return "character1";
+		return "killer";
 	}
 
-	@GetMapping("killer.html")
+	//@GetMapping("killer.html")
 	public String killer(){
 		return "killer";
 	}
