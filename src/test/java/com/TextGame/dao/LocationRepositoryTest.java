a5c785988e3be9ea4cf75ac3d356a506918d96e8
NataliiaAmons <148610267+NataliiaAmons@users.noreply.github.com>
2023-12-03 11:55:30 +0200

    fixed locations test

--- src/test/java/com/TextGame/dao/LocationRepositoryTest.java
+++ src/test/java/com/TextGame/dao/LocationRepositoryTest.java
@@ -1,6 +1,7 @@
     package com.TextGame.dao;
 
 import com.TextGame.viewmodel.LocationVM;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,19 +15,23 @@ import com.TextGame.dao.LocationRepository;
 
     public class LocationRepositoryTest {
 
-        @Autowired
         private LocationRepository locationRepository;
+
+        @BeforeEach
+        public void setUp() {
+            locationRepository = new LocationRepository();
+        }
 
         @Test
         public void LocationRepositoryTest_getAllLocations() throws IOException {
             ArrayList<LocationVM> allLocations = locationRepository.getAllLocations();
             assertEquals(allLocations.get(0).getNumber(), 1);
             assertEquals(allLocations.get(0).getPrevious(), 0);
-            assertEquals(allLocations.get(0).getName(), is("Тіло"));
-            assertEquals(allLocations.get(0).getText(), is("текст1"));
-            assertEquals(allLocations.get(0).getNumber(), 4);
-            assertEquals(allLocations.get(0).getPrevious(), 2);
-            assertEquals(allLocations.get(0).getName(), is("Армстрон"));
-            assertEquals(allLocations.get(0).getText(), is("текст4"));
+            assertEquals(allLocations.get(0).getName(), "Тіло");
+            assertEquals(allLocations.get(0).getText(), " ");
+            assertEquals(allLocations.get(3).getNumber(), 4);
+            assertEquals(allLocations.get(3).getPrevious(), 2);
+            assertEquals(allLocations.get(3).getName(), "Армстронг");
+            assertEquals(allLocations.get(3).getText(), "текст");
         }
     }
